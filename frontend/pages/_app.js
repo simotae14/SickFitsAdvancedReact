77b51552bb3cd013b503282fe809a3a99797b39d
simotae14 <startae14@gmail.com>
2020-03-04 00:28:47 +0100

    added custom App component

---
+++ frontend/pages/_app.js
@@ -0,0 +1,19 @@
+import App, { Container } from "next/app";
+
+class MyApp extends App {
+    render() {
+        const {
+            Component
+        } = this.props;
+        return (
+            <Container>
+                <p>
+                    Hey, I am in every page!
+                </p>
+                <Component />
+            </Container>
+        );
+    }
+}
+
+export default MyApp;
