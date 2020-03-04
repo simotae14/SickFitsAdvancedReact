572089a3d55877370de1ae2a6a3160c936f758d9
simotae14 <startae14@gmail.com>
2020-03-04 01:12:18 +0100

    used some styled components

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import Header from "../components/Header";
 import Meta from "../components/Meta";
+import styled from "styled-components";
+
+const MyButton = styled.button`
+    background: red;
+    font-size: ${props => props.huge ? '100px' : '50px'};
+    .poop {
+        font-size: 100px;
+    }
+`;
 
 class Page extends Component {
     render() {
@@ -8,6 +17,14 @@ class Page extends Component {
             <div>
                 <Meta />
                 <Header />
+                <MyButton huge>
+                    Click me
+                    <span className="poop">💩</span>
+                </MyButton>
+                <MyButton>
+                    Click me
+                    <span className="poop">💩</span>
+                </MyButton>
                 {this.props.children}
             </div>
         );
