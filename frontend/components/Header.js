d6ba961933839a73f4c561e272111a4faf8ff6fc
simotae14 <startae14@gmail.com>
2020-03-06 00:11:00 +0100

    add Nprogress bar

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,6 +1,21 @@
 import styled from "styled-components";
 import Link from "next/link";
+import Router from "next/router";
+import NProgress from "nprogress";
+
 import Nav from "../components/Nav";
+
+Router.onRouteChangeStart = () => {
+    NProgress.start();
+};
+
+Router.onRouteChangeComplete = () => {
+    NProgress.done();
+};
+
+Router.onRouteChangeError = () => {
+    NProgress.done();
+};
 
 const Logo = styled.h1`
     font-size: 4rem;
