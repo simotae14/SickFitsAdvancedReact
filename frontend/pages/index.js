ef22da07fdb3b4debf5eaed8e950a936ee610925
Simone <startae14@gmail.com>
2021-02-16 17:18:08 +0100

    04. create the _app page shared by all the components

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,9 +2,10 @@ import Page from '../components/Page';
 
 export default function IndexPage() {
   return (
-    <Page cool="Heck ye!">
+    <>
       <p>Hello!</p>
       <code>var</code>
-    </Page>
+      <p>I am a child</p>
+    </>
   );
 }
--- frontend/pages/sell.js
+++ frontend/pages/sell.js
@@ -1,9 +1,3 @@
-import Page from '../components/Page';
-
 export default function SellPage() {
-  return (
-    <Page>
-      <p>Hello Sell!</p>
-    </Page>
-  );
+  return <p>Hello Sell!</p>;
 }
