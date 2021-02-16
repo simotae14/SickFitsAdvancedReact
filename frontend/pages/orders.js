0f8454e5f1059d9a0859be768e4bcee8ca11dda2
Simone <startae14@gmail.com>
2021-02-16 16:52:30 +0100

    02. create some pages with next.js

---
+++ frontend/pages/orders.js
@@ -0,0 +1,7 @@
+export default function OrdersPage() {
+  return (
+    <div>
+      <p>Hello Orders!</p>
+    </div>
+  );
+}
---
+++ frontend/pages/sell.js
@@ -0,0 +1,7 @@
+export default function SellPage() {
+  return (
+    <div>
+      <p>Hello Sell!</p>
+    </div>
+  );
+}
