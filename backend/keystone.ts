b4ef78f5d30f1276159565413b2be887858a9dd4
Simone <startae14@gmail.com>
2021-03-06 20:44:31 +0100

    13. Setting up Keystone and Typescript to run the backend part

---
+++ backend/keystone.ts
@@ -0,0 +1,33 @@
+import 'dotenv/config';
+import { config, createSchema } from '@keystone-next/keystone/schema';
+
+const databaseURL =
+  process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
+
+// session config for how long a user stayed signin
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360,
+  secret: process.env.COOKIE_SECRET,
+};
+
+export default config({
+  server: {
+    cors: {
+      origin: [process.env.FRONTEND_URL],
+      credentials: true,
+    },
+  },
+  db: {
+    adapter: 'mongoose',
+    url: databaseURL,
+    // TODO: add data seeding here
+  },
+  lists: createSchema({
+    // Schema items going here
+  }),
+  ui: {
+    // TODO: change this for roles
+    isAccessAllowed: () => true,
+  },
+  // TODO: add session values here
+});
