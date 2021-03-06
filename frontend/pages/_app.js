981d2804b4ca967f4882028c4eed4a625d60ffa3
Simone <startae14@gmail.com>
2021-03-06 19:37:53 +0100

    10. use the nProgress library to show a red progress bar when we refresh or load a page

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,4 +1,15 @@
+import NProgress from 'nprogress';
+import Router from 'next/router';
 import Page from '../components/Page';
+
+import '../components/styles/nprogress.css';
+
+// Start di NProgress Bar
+Router.events.on('routeChangeStart', () => NProgress.start());
+// Stop di NProgress Bar
+Router.events.on('routeChangeComplete', () => NProgress.done());
+// Stop di NProgress Bar in case of Error
+Router.events.on('routeChangeError', () => NProgress.done());
 
 export default function MyApp({ Component, pageProps }) {
   return (
